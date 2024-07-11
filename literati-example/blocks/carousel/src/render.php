297a513bd9c2d137ff8af17c6fb9e756b50dbd30
brendansmith <brendan.smith0325@gmail.com>
2024-07-11 17:19:21 -0400

    Get basic carousel working using splide.js library

--- literati-example/blocks/carousel/src/render.php
+++ literati-example/blocks/carousel/src/render.php
@@ -2,7 +2,38 @@
 /**
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
+$args = [
+    'post_type' => 'literati_promotion',
+    'posts_per_page' => 10,
+];
+$promotions = get_posts($args);
 ?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Literati Example Carousel – hello from a dynamic block!', 'literati-example-carousel' ); ?>
-</p>
+<div <?php echo get_block_wrapper_attributes(); ?>>
+    <?php if (count($promotions)) { ?>
+        <div class="splide" data-interval="<?php echo $attributes['transitionTimer']?>">
+            <div class="splide__track">
+                <ul class="splide__list">
+                    <?php foreach( $promotions as $promotion) { ?>
+                    <!-- add slide -->
+                        <li class="splide__slide">
+                            <article class="card">
+                                <header>
+                                    <h2><?php echo get_post_meta($promotion->ID, '_literati_promotion_header', true) ?></h2>
+                                </header>
+                                <img src="<?php echo get_the_post_thumbnail_url($promotion) ?>" alt="<?php echo get_post_meta( get_post_thumbnail_id($promotion->ID), '_wp_attachment_image_alt', true); ?>">
+                                <div class="content">
+                                    <p><?php echo get_post_meta($promotion->ID, '_literati_promotion_text', true) ?></p>
+                                </div>
+                                <a class="carousel-button" href="<?php echo get_permalink($promotion->ID) ?>"><?php echo get_post_meta($promotion->ID, '_literati_promotion_button', true) ?></a>
+                            </article>
+                        </li>
+                      <?php
+                    }
+                    ?>
+                </ul>
+            </div>
+        </div>
+    <?php } else { ?>
+      No Promotions
+    <?php } ?>
+</div>
--- literati-example/blocks/carousel/src/view.js
+++ literati-example/blocks/carousel/src/view.js
@@ -21,5 +21,26 @@
  */
 
 /* eslint-disable no-console */
-console.log( 'Hello World! (from create-block-literati-example-carousel block)' );
+import Splide from '@splidejs/splide';
+
+// Import our default css theme for splide
+import '@splidejs/splide/css';
+
+// This should only be output on the frontend of the site - NOT the editor
+console.log("hello from the frontend.")
+
+document.addEventListener( 'DOMContentLoaded', () => {
+
+    // Splide
+    const splides = document.querySelectorAll('.splide')
+
+    if(!!splides && splides.length > 0){
+        splides.forEach(splide => {
+            // Grab the slide interval and convert it to an integer and milliseconds
+            const interval = parseInt(splide.dataset.interval) * 1000
+
+            new Splide(splide, {type: 'loop', autoplay: true, perPage: 3, pauseOnHover: false, interval: interval}).mount()
+        })
+    }
+})
 /* eslint-enable no-console */
